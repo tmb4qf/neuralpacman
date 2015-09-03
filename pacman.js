5451b2f3a24dfa230768cb6e6dacc197a8338dac
Tyler Brown <tmb4qf@mail.missouri.edu>
2015-09-02 21:37:41 -0500

    Set up different pacman modes

--- pacman.js
+++ pacman.js
@@ -4,10 +4,17 @@ var BLINKY_START = 405.5;
 var PINKY_START = 489.5;
 var INKY_START = 487.5;
 var CLYDE_START = 491.5;
+
 var UP = 1;
 var RIGHT = 2;
 var DOWN = 3;
 var LEFT = 4;
+
+var CHASE = 1;
+var SCATTER = 2;
+var FRIGHTENED = 3;
+
+var timing = [7,20,7,20,5,20,5];
 var div = document.getElementById('canvas');
 var game1;
 
@@ -18,14 +25,15 @@ function Location(x,y)
 }
 
 function Game(){	
-	this.pacman = new Agent(squareToPixels(PACMAN_START), 0, 0, '#FFFB14', 0);
-	this.blinky = new Agent(squareToPixels(BLINKY_START), 0, 0, '#FF1212',0);
-	this.pinky = new Agent(squareToPixels(PINKY_START), 0, 0, '#FFC9DC',0);
-	this.inky = new Agent(squareToPixels(INKY_START), 0, 0, '#A8FFFE',0);
-	this.clyde = new Agent(squareToPixels(CLYDE_START), 0, 0, '#FCC78D',0);
+	this.pacman = new Agent(squareToPixels(PACMAN_START), 80, 0, '#FFFB14', 0, pacmanTrav);
+	this.blinky = new Agent(squareToPixels(BLINKY_START), 75, 0, '#FF1212',0, blinkyTrav);
+	this.pinky = new Agent(squareToPixels(PINKY_START), 75, 0, '#FFA8C7',0, pinkyTrav);
+	this.inky = new Agent(squareToPixels(INKY_START), 75, 0, '#78FFFE',0, inkyTrav);
+	this.clyde = new Agent(squareToPixels(CLYDE_START), 75, 0, '#FFC17D',0, clydeTrav);
 	
 	this.dots;
 	this.energizers;
+	this.mode;
 }
 
 Game.prototype.updateAgentsLocation = function(){
@@ -44,33 +52,53 @@ Game.prototype.drawAgents = function(){
 	this.clyde.draw();
 }
 
+Game.prototype.changeMode = function(mode){
+	this.mode = mode;
+	this.pacman.mode = mode;
+	this.blinky.mode = mode;
+	this.pinky.mode = mode;
+	this.inky.mode = mode;
+	this.clyde.mode = mode;
+	
+	if(mode == FRIGHTENED){
+		this.blinky.color = '#2F1CFF';
+		this.pinky.color = '#2F1CFF';
+		this.inky.color = '#2F1CFF';
+		this.clyde.color = '#2F1CFF';
+		
+		this.blinky.velocity = 50;
+		this.pinky.velocity = 50;
+		this.inky.velocity = 50;
+		this.clyde.velocity = 50;
+	}
+	else if(mode == CHASE || mode == SCATTER){
+		this.blinky.color = '#FF1212';
+		this.pinky.color = '#FFA8C7';
+		this.inky.color = '#78FFFE';
+		this.clyde.color = '#FFC17D';
+		
+		this.blinky.velocity = 75;
+		this.pinky.velocity = 75;
+		this.inky.velocity = 75;
+		this.clyde.velocity = 75;
+	}
+}
 
-function Agent(location, velocity, direction, color, state){
+
+function Agent(location, velocity, direction, color, mode, traversal){
 	this.location = location;
 	this.velocity = velocity;
 	this.direction = direction;
 	this.color = color;
-	this.state = state;
+	this.mode = mode;
+	this.traversal = traversal;
 }
 
 Agent.prototype.updateLocation = function(){
-	switch(this.direction){
-		case UP:
-			this.location.x -= this.velocity * .03;
-			break;
-		case DOWN: 
-			this.location.x += this.velocity * .03;
-			break;
-		case RIGHT:
-			this.location.y += this.velocity * .03;
-			break;
-		case LEFT:
-			this.location.y -= this.velocity * .03;
-			break;
-	}
+	this.traversal(this.location, this.direction, this.velocity);
 };
 
-Agent.prototype.draw = function(location){
+Agent.prototype.draw = function(){
 	fill(this.color);
 	noStroke();
 	ellipse(this.location.x, this.location.y, 28, 28);
@@ -97,6 +125,63 @@ function setup(){
 	game1.drawAgents();
 }
 
+function startGame(){
+	scatterMode();
+}
+
+function chaseMode(){
+	if(timing.length > 0){
+		setTimeout(scatterMode, timing[0] * 1000);
+		game1.changeMode(CHASE);
+		timing.splice(0,1);
+	}
+	else
+	{
+		game1.changeMode(CHASE);
+	}
+}
+
+function scatterMode(){
+	if(timing.length > 0){
+		setTimeout(chaseMode, timing[0] * 1000);
+		game1.changeMode(SCATTER);
+		timing.splice(0,1);
+	}
+}
+
+
+function pacmanTrav(location, direction, velocity){
+	switch(direction){
+		case UP:
+			location.x -= velocity * .03;
+			break;
+		case DOWN: 
+			location.x += velocity * .03;
+			break;
+		case RIGHT:
+			location.y += velocity * .03;
+			break;
+		case LEFT:
+			location.y -= velocity * .03;
+			break;
+	}
+}
+
+function blinkyTrav(){
+	
+}
+
+function pinkyTrav(){
+	
+}
+
+function inkyTrav(){
+	
+}
+
+function clydeTrav(){
+	
+}
 
 //event listener for arrow keys
 document.addEventListener("keydown", function(e){
@@ -117,6 +202,9 @@ document.addEventListener("keydown", function(e){
 		case 40:
 			game1.pacman.direction = RIGHT;
 			game1.pacman.velocity = 80;
+		break;
+		case 13:
+			game1.pacman.velocity = 0;
 		break;
 	}
 });
